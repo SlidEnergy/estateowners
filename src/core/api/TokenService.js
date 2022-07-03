020df54c700027a6bc1dd97bd05a4dbb67623524
slidenergy <slidenergy@gmail.com>
2022-07-03 22:51:55 +0300

    fix api url

--- src/core/api/TokenService.js
+++ src/core/api/TokenService.js
@@ -2,7 +2,7 @@ import {http} from "../http-common";
 
 export default class TokenService {
     static async get(email, password) {
-        const response = await http.post('/api/v1/token', {email, password});
+        const response = await http.post('/token', {email, password});
 
         return response.data;
     }
