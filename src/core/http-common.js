03d730254f4b503c07f0c5188c0c6d2b7b0d01db
slidenergy <slidenergy@gmail.com>
2022-07-03 23:17:47 +0300

    add evironment variable, remove proxy

--- src/core/http-common.js
+++ src/core/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const { REACT_APP_BASE_API_URL } = process.env;
 
 export const http = axios.create({
-    baseURL: '/api/v1',
+    baseURL: `${REACT_APP_BASE_API_URL}/api/v1`,
     headers : {
         'content-type': 'application/json'
     }
